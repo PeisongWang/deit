b0b68a6a93c80737216f212527d61757b1553244
Peisong Wang <peisong.wang@nlpr.ia.ac.cn>
2022-07-15 13:59:55 +0800

    random->unstructured

--- models.py
+++ models.py
@@ -267,7 +267,7 @@ def masked_deit_small_patch16_224(pretrained=False, **kwargs):
     return model
 
 @register_model
-def random_masked_deit_small_patch16_224(pretrained=False, **kwargs):
+def unstructured_masked_deit_small_patch16_224(pretrained=False, **kwargs):
     model = UnstructuredMaskedVisionTransformer(
         patch_size=16, embed_dim=384, depth=12, num_heads=6, mlp_ratio=4, qkv_bias=True,
         norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
